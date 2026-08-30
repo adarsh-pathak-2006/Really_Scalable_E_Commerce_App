ee551d5d95dba69c866ce06d809c4bccf03be1e2
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-31 00:39:57 +0530

    added models in the products and now moving to setup models in the orders app

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,4 @@
 from django.db import models
+from authentication.models import Profile
+from products.models import Product
 
-# Create your models here.
--- products/models.py
+++ products/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
+class Category(models.Model):
+    name=models.CharField(max_length=150)
+    description=models.TextField()
+    is_active=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name=models.CharField(max_length=200)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
     thumbnail=models.ImageField(upload_to='product-pics/')
     price=models.PositiveIntegerField()
     stock=models.PositiveIntegerField()
+    is_avaliable=models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.stock > 0:
+            self.is_avaliable=True
+        else:
+            self.is_avaliable=False
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
