435662cc2b8834cfcf51be6f75a450980c787bf7
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-30 19:05:45 +0530

    added models in the authentication app

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES=[('CUSTOMER', 'Customer'), ('ADMIN', 'Admin')]
+    role=models.CharField(max_length=10, choices=ROLE_CHOICES, default='CUSTOMER')
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    bio=models.TextField()
+    created_on=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
