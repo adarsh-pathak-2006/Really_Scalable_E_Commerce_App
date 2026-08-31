5376bfd222c4156891bba4e58fb5407e24bb9d0a
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-31 16:23:55 +0530

    added serializer in products too

---
+++ products/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from .models import Category, Product
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model=Category
+        fields='__all__'
+
+class ProductSerializer(ModelSerializer):
+    category=PrimaryKeyRelatedField(queryset=Category.objects.all())
+    class Meta:
+        model=Product
+        fields='__all__'
+        read_only_fields=['is_avaliable']
