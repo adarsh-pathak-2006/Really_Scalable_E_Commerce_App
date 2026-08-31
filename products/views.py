2e8cdf2b9f852b6f5a0ff7951fcb8c68730927ac
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-31 18:21:34 +0530

    added views in the products app too

--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class CategoryAPI(ListCreateAPIView):
+    serializer_class=CategorySerializer
+    queryset=Category.objects.all()
+
+class CategoryDetailAPI(RetrieveUpdateDestroyAPIView):
+    serializer_class=CategorySerializer
+    queryset=Category.objects.all()
+
+class ProductAPI(ListCreateAPIView):
+    serializer_class=ProductSerializer
+    queryset=Product.objects.all()
+
+class ProductDetailAPI(RetrieveUpdateDestroyAPIView):
+    serializer_class=ProductSerializer
+    queryset=Product.objects.all()
