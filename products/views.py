196f16b783d978f27dc54d9314ba3ebbbcff3876
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-31 19:54:47 +0530

    added pagination and also cached the paginated response

--- products/views.py
+++ products/views.py
@@ -2,21 +2,40 @@ from django.shortcuts import render
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from e_commerce_backend.pagination import GeneralPagination
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 class CategoryAPI(ListCreateAPIView):
     serializer_class=CategorySerializer
     queryset=Category.objects.all()
+    pagination_class=GeneralPagination
+
+    @method_decorator(cache_page(60*5))
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
 class CategoryDetailAPI(RetrieveUpdateDestroyAPIView):
     serializer_class=CategorySerializer
     queryset=Category.objects.all()
 
+    @method_decorator(cache_page(60*5))
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
 class ProductAPI(ListCreateAPIView):
     serializer_class=ProductSerializer
     queryset=Product.objects.all()
+    pagination_class=GeneralPagination
+
+    @method_decorator(cache_page(60*5))
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
 class ProductDetailAPI(RetrieveUpdateDestroyAPIView):
     serializer_class=ProductSerializer
     queryset=Product.objects.all()
+
+    @method_decorator(cache_page(60*5))
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
