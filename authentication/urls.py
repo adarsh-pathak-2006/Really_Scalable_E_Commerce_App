bbec4a68ad806bba3cea3c37c250a2666d921917
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-01 12:40:03 +0530

    added all the urls in the project as well in the apps

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import RegisterAPI, MyProfileAPI
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterAPI.as_view()),
-    path('my-profile/', MyProfileAPI.as_view())
+    path('my-profile/', MyProfileAPI.as_view()),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
